4afc68ca69fd68de8c1ceffd77c360eb9e92dfc3
Sarah Thiery <sarahvthiery@gmail.com>
2021-08-18 12:09:36 -0700

    commit tree 2bddc7f3331f613ae1a4e82d8bdeb48fbe012e5c

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, minIndex + 1);
+  return min;
 }
 
-function selectionSort(array){
-
+function selectionSort(array) {
+  let newArray = [];
+  while (array.length > 0) {
+    newArray.push(findMinAndRemove(array));
+  }
+  return newArray;
 }
